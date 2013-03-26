bf7c4f0f4c134718052621971450368248dcb5d0
Moritz Schlarb <moschlar@metalabs.de>
2013-03-27 00:12:01 +0100

    No echo in test cases

--- tests/test_model.py
+++ tests/test_model.py
@@ -13,7 +13,7 @@ class TestModel(unittest.TestCase):
 
     def _session_(self):
         model = self._import_()
-        engine = create_engine('sqlite:///:memory:', echo=True)
+        engine = create_engine('sqlite:///:memory:')
         model.Session.configure(bind=engine)
         model.metadata.create_all(engine)  # @UndefinedVariable
         return model.Session()
@@ -25,14 +25,14 @@ class TestModel(unittest.TestCase):
     def test_create(self):
         '''Model metadata can be created'''
         from noodle.core.model import Session, metadata
-        engine = create_engine('sqlite:///:memory:', echo=True)
+        engine = create_engine('sqlite:///:memory:')
         Session.configure(bind=engine)
         metadata.create_all(engine)  # @UndefinedVariable
 
     def test_query(self):
         '''Model can be queried'''
         from noodle.core.model import Session, metadata, Share
-        engine = create_engine('sqlite:///:memory:', echo=True)
+        engine = create_engine('sqlite:///:memory:')
         Session.configure(bind=engine)
         metadata.create_all(engine)  # @UndefinedVariable
         Session().query(Share).all()
