744faa95bf0ba2dce4b508963bfe5cf54f77b05f
Moritz Schlarb <moschlar@metalabs.de>
2013-03-26 17:51:26 +0100

    Working plugin detection - Try it out with noodle.plugins.pysmbc

---
+++ noodle/core/plugins.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+""""""
+
+import pkg_resources
+
+
+if __name__ == '__main__':
+    for entrypoint in pkg_resources.iter_entry_points('noodle.plugins'):
+        print entrypoint
+        # Grab the function that is the actual plugin.
+        plugin = entrypoint.load()  # Call the plugin
+        print plugin
+        print dir(plugin)
+        try:
+            print plugin.hello
+        except:
+            pass
