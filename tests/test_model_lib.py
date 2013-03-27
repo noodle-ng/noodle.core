4a303563fed9281fcd280955f8cb78119dba9ee2
Moritz Schlarb <moschlar@metalabs.de>
2013-03-27 13:20:59 +0100

    Add test cases for postgresql IPAddress

--- tests/test_model_lib.py
+++ tests/test_model_lib.py
@@ -4,6 +4,7 @@ import unittest
 
 from noodle.core.model.lib import IPAddress
 from sqlalchemy.dialects.sqlite.base import SQLiteDialect
+from sqlalchemy.dialects.postgresql.base import PGDialect
 
 
 class TestIPAddress(unittest.TestCase):
@@ -20,4 +21,14 @@ class TestIPAddress(unittest.TestCase):
     def test_int2ip(self):
         '''IPAddress int2ip'''
         addr = self.ipaddress.process_result_value(16712194, SQLiteDialect)
+        self.assertEqual(addr, '1.2.3.4')
+
+    def test_ip2int_postgresql(self):
+        '''IPAddress ip2int for postgresql'''
+        addr = self.ipaddress.process_bind_param('1.2.3.4', PGDialect)
+        self.assertEqual(addr, '1.2.3.4')
+
+    def test_int2ip_postgresql(self):
+        '''IPAddress int2ip for postgresql'''
+        addr = self.ipaddress.process_result_value('1.2.3.4', PGDialect)
         self.assertEqual(addr, '1.2.3.4')
